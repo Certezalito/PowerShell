f8c8f6bd1661cd23e779b5880fa05ec121781025
John <49564480+XzeroR3@users.noreply.github.com>
2022-07-11 20:14:26 +0000

    Initial Commit

---
+++ Utility/get-filehashes.ps1
@@ -0,0 +1,8 @@
+# Script to take a file and get all available hashes based on the available hashing algorithms listed in the help file 
+
+# Get the file to hash 
+$cessnawhat = Get-item 'S:\Downloads Browser\755506217547202751.webp'
+# Get the available hashing algorithms
+$algos =  Get-Help Get-FileHash -Parameter algorithm | Out-String -Stream | Select-String "-"
+# Parse the help file for all the available algorithms and feed them to get-filehash
+$algos -match "- " -replace "    - " | foreach {Get-FileHash $cessnawhat -Algorithm $_ } | ft algorithm, hash -AutoSize
