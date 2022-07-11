abe24425e98473baa5cd6aaf6f740fef2c3a46b6
John <49564480+Certezalito@users.noreply.github.com>
2022-07-11 20:15:40 +0000

    Update get-filehashes.ps1
    cleanup text

--- Utility/get-filehashes.ps1
+++ Utility/get-filehashes.ps1
@@ -2,7 +2,7 @@
 
 # Get the file to hash 
 $cessnawhat = Get-item 'S:\Downloads Browser\755506217547202751.webp'
-# Get the available hashing algorithms
-$algos =  Get-Help Get-FileHash -Parameter algorithm | Out-String -Stream | Select-String "-"
+# Get the available hashing algorithms from the help file
+$algos = Get-Help Get-FileHash -Parameter algorithm | Out-String -Stream | Select-String "-"
 # Parse the help file for all the available algorithms and feed them to get-filehash
 $algos -match "- " -replace "    - " | foreach {Get-FileHash $cessnawhat -Algorithm $_ } | ft algorithm, hash -AutoSize
